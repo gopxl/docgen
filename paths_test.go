e989d29a53c810b7aa87c92ec549586eed42e311
Mark Kremer <mail@markkremer.nl>
2024-10-20 18:35:57 +0200

    Update tests

--- paths_test.go
+++ paths_test.go
@@ -6,32 +6,13 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestSectionDirectoryRenamer_Rename(t *testing.T) {
-	r := SectionDirectoryRenamer{}
+func TestPathRewriter_ModifyPath(t *testing.T) {
+	r := PathRewriter{}
 
-	// Untouched (except slugification)
-	assert.Equal(t, "tutorial/Foo.md", r.Rename("tutorial/Foo.md"))
-	assert.Equal(t, "1-tutorial/Foo.md", r.Rename("1 Tutorial/Foo.md"))
-	assert.Equal(t, "tutorial/2. Foo.md", r.Rename("Tutorial/2. Foo.md"))
-	assert.Equal(t, "tutorial/Foo.md", r.Rename(".Tutorial/Foo.md"))
-	assert.Equal(t, "tutorial/01. Subdir/Foo.md", r.Rename("Tutorial/01. Subdir/Foo.md"))
+	assert.Equal(t, "tutorial/foo.html", r.ModifyPath("tutorial/Foo.md", false))
+	assert.Equal(t, "tutorial/foo.html", r.ModifyPath("01. Tutorial/01. Foo.md", false))
+	assert.Equal(t, "tutorial", r.ModifyPath("01. Tutorial", true))
 
-	// Renamed
-	assert.Equal(t, "tutorial/2. Foo.md", r.Rename("1. Tutorial/2. Foo.md"))
-	assert.Equal(t, "tutorial/02. Foo.md", r.Rename("01. Tutorial/02. Foo.md"))
-	assert.Equal(t, "first-steps-and-introduction/Foo.md", r.Rename("First Steps & introduction/Foo.md"))
-	assert.Equal(t, "tutorial/images/foo.png", r.Rename("01. Tutorial/images/foo.png"))
-}
-
-func TestPageFileRenamer_Rename(t *testing.T) {
-	r := PageFileRenamer{}
-
-	// Untouched
-	assert.Equal(t, "Tutorial/foo.md", r.Rename("Tutorial/foo.md"))
-	assert.Equal(t, "01. Tutorial/foo.md", r.Rename("01. Tutorial/foo.md"))
-
-	// Renamed
-	assert.Equal(t, "Tutorial/foo.md", r.Rename("Tutorial/01. Foo.md"))
-	assert.Equal(t, "Tutorial/foo-bar.md", r.Rename("Tutorial/01. Foo Bar.md"))
-	assert.Equal(t, "Tutorial/1-foo.md", r.Rename("Tutorial/1 Foo.md"))
+	assert.Equal(t, "tutorial/foo.html", r.ModifyPath("tutorial/foo.html", false))
+	assert.Equal(t, "tutorial/01. Images/01. foo.png", r.ModifyPath("01. Tutorial/01. Images/01. foo.png", false))
 }
